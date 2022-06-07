1c081e7b2e5aeabd6e0e85aead3124da3a9a21aa
carlosmz87 <carlosmartz1995@gmail.com>
2022-06-06 22:30:07 -0600

    (add) environments preparing tests

--- Backend/src/rutas.js
+++ Backend/src/rutas.js
@@ -12,7 +12,7 @@ const mysqlCon = mysql.createConnection({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    port: "3306",
+    port: process.env.DATABASE_PORT,
     ssl: {
         rejectUnauthorized: false
       }
