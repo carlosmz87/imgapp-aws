aaca1449abecf7d27bf01b40bebf5d6547804914
carlosmz87 <carlosmartz1995@gmail.com>
2022-07-18 10:09:06 -0600

    test healthcheck route frontend

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HealthcheckComponent } from './healthcheck/healthcheck.component';
+import { FrontpageComponent } from './views/frontpage/frontpage.component';
 
 const routes: Routes = [
-  { path: 'health', component: HealthcheckComponent }
+  { 
+    path: 'health', 
+    component: HealthcheckComponent 
+  },
+  {
+    path: '',
+    component: FrontpageComponent,
+    pathMatch: 'full'
+  
+  }
 ];
 
 @NgModule({
